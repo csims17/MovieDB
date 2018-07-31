13b35709563201599acd39c8bdd252a8eac0d9ea
NickStafford2 <41924040+NickStafford2@users.noreply.github.com>
2018-07-31 16:08:39 -0400

    made minor changes to my config
    This file should be unique to each local server. how do we add this file to gitignore?
    removed some comments.

--- src/application/config/config.php
+++ src/application/config/config.php
@@ -1,23 +1,14 @@
 <?php
 	
-	//echo __DIR__;
-
 	return array(
-		'ROOT_URI' 		=> "/~nstaffo2/",					// this must be the url on your machine that precedes index.php
+		'ROOT_URI' 		=> "/",					// this must be the url on your machine that precedes index.php
 		'DB_SERVER' 	=> "localhost",						
-		'DB_USERNAME' 	=> "nstaffo2", 				
-		'DB_PASSWORD' 	=> "yt2aP2yJ", 				
+		'DB_USERNAME' 	=> "root", 				
+		'DB_PASSWORD' 	=> "9UfhC%DHbC.G=tY.", 				
 		'DB_NAME' 		=> "nstaffo2",
 		'DB_PORT'		=> "3306",
 		'DB_CHARSET'	=> "3306"
 		 );
 
-	//echo "configure routes...";
-	//require_once('/home/nstaffo2/public_html/routes.php');
-	/*
-	echo "configure DB...";
-	require_once($ROOT . 'database/config.php');
-	echo "root is : " . $ROOT;
 	echo "Configuration complete.";
-	*/
 ?>
