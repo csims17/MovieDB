7ce6a4a3d4ae026ea24ae297b7671535b355c0ae
tbingha3 <41810534+tbingha3@users.noreply.github.com>
2018-08-01 20:53:40 -0400

    Create ReviewController.class.php

---
+++ src/application/controllers/ReviewController.class.php
@@ -0,0 +1,12 @@
+<?php
+/*class ReviewController extends Controller{
+
+public function changeRating($parameters) {
+$testModel = new ReviewModel("reviews");
+
+echo ReviewModel->setRating("7", "movieguy12", "1");
+
+} //end of changeRating
+*/
+}
+?>
