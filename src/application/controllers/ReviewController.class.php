b01696ad17181e4dfb143d7d930509e3a1b33fac
tbingha3 <41810534+tbingha3@users.noreply.github.com>
2018-08-03 08:28:24 -0400

    Update ReviewController.class.php

--- src/application/controllers/ReviewController.class.php
+++ src/application/controllers/ReviewController.class.php
@@ -1,12 +1,17 @@
+//ReviewController.php
+
 <?php
-/*class ReviewController extends Controller{
+class ReviewController extends Controller{
 
-public function changeRating($parameters) {
-$testModel = new ReviewModel("reviews");
 
-echo ReviewModel->setRating("7", "movieguy12", "1");
+public function changeRating($user, $movieID, $rating){ //We need a username and movie ID to uniquely identify a review, and a rating to assign
+	$testModel = new ReviewModel("reviews");
 
-} //end of changeRating
-*/
-}
+	//echo ReviewModel->setRating("7", "movieguy12", "1"); //Each review needs a rating, a username, and a movie ID
+
+}	//end of changeRating
+
+} //end of class
+
+
 ?>
