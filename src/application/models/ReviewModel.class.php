e2acc38d4a57827acbad80c48f6cfdf68b7d14ff
tbingha3 <41810534+tbingha3@users.noreply.github.com>
2018-08-03 08:29:37 -0400

    Update ReviewModel.class.php

--- src/application/models/ReviewModel.class.php
+++ src/application/models/ReviewModel.class.php
@@ -10,9 +10,14 @@ class ReviewModel extends Model {
   $sql .= ";";
     
     $result = $this->query($sql);
+    echo $result;
     return $result;
   }
  //public function getPaginatedReviews() {}
-  //public function getReviews($username) {}
+  public function getReviews($username) {
+  	$sql = "SELECT * FROM $this->table limit 50 WHERE username = " . $username;
+    $result = $this->query($sql);
+    return $result; 
+  }
 }
 ?>
