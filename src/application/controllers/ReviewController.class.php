986266ba55361c18cfe7392e63bd6b54a85aa096
tbingha3 <41810534+tbingha3@users.noreply.github.com>
2018-08-03 14:51:04 -0400

    Update ReviewController.class.php

--- src/application/controllers/ReviewController.class.php
+++ src/application/controllers/ReviewController.class.php
@@ -9,7 +9,27 @@ public function changeRating($user, $movieID, $rating){ //We need a username and
 $testModel->setRating($user, $movieID, $rating);
 	//echo testModel->setRating($user, $movieID, $rating); //Each review needs a rating, a username, and a movie ID
 
+ include CURR_VIEW_PATH . "reviews" . DS . "index.php";
 }	//end of changeRating
+
+	
+
+public function singleLookUp($user, $movieID){
+$testModel = new ReviewModel("reviews");
+
+$testModel->getSingleReview($user, $movieID);
+
+include CURR_VIEW_PATH . "reviews" . DS . "index.php"; 
+}//end of singleLookUp
+
+public function LookUpAll($user){
+$testModel = new ReviewModel("reviews");
+
+$testModel->getReviews($user);
+
+include CURR_VIEW_PATH . "reviews" . DS. "index.php";
+
+}//end of LookUpAll
 
 } //end of class
 
